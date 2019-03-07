2901597803651bf22a6ccc62f53d7317ce32d476
devbysalas <rgkalin@gmail.com>
2019-03-07 12:11:45 -0800

    Working nav and hero section

--- front-page.php
+++ front-page.php
@@ -19,26 +19,5 @@ $container = get_theme_mod('eventosybodas_container_type');
 <!-- Hero Section/Jumbotron -->
 <?php get_template_part('global-templates/jumbotron'); ?>
 
-<!-- Sign up Section -->
-<?php get_template_part('global-templates/signup'); ?>
-
-<!-- Latest Section -->
-<?php get_template_part('global-templates/latest'); ?>
-
-<!-- Quote Section -->
-<?php get_template_part('global-templates/quote'); ?>
-
-<!-- Book Section -->
-<?php get_template_part('global-templates/book'); ?>
-
-<!-- Live Section -->
-<?php get_template_part('global-templates/live'); ?>
-
-<!-- Biography Section -->
-<?php get_template_part('global-templates/biography'); ?>
-
-<!-- Gallery Section -->
-<?php get_template_part('global-templates/gallery'); ?>
-
 
 <?php get_footer(); ?>
