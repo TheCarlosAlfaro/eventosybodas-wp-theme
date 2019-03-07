dbc30cb095c0c606eedad84b2cb84fb25a590482
devbysalas <rgkalin@gmail.com>
2019-03-07 12:26:50 -0800

    Adds responsive typografy

--- global-templates/jumbotron.php
+++ global-templates/jumbotron.php
@@ -12,9 +12,9 @@ if (!defined('ABSPATH')) {
 ?>
 
 <div class="jumbotron jumbotron-fluid text-center hero--bg-image text-light" style="background-image: 
-    linear-gradient(rgba(20,20,20, .5), rgba(20,20,20, .5)), url('<?php the_field('hero_image'); ?>');">
+    linear-gradient(rgba(20,20,20, .3), rgba(20,20,20, .3)), url('<?php the_field('hero_image'); ?>');">
   <div class="container hero-content">
-    <h1 class="display-4 text-uppercase mb-5">The perfect backdrop for your event with dramatic downtown views and
+    <h1 class="text-uppercase mb-5">The perfect backdrop for your event with dramatic downtown views and
       unrivaled
       services.</h1>
 
